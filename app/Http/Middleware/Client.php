fa9e847b6a31094b1ad39b0b82832471f20bbc9a
Kataleko <katalekoweb@gmail.com>
2025-08-22 17:30:06 +0100

    chore: create client middleware

---
+++ app/Http/Middleware/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Client
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->user() and !$request->user()->is_admin)
+            return $next($request);
+        return redirect()->route('dashboard')->with('error', 'Voce não tem permissão para acessar esta página.');
+    }
+}
