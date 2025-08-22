0721a713283771d755bef7fb4835a6e09a310658
Kataleko <katalekoweb@gmail.com>
2025-08-22 18:26:24 +0100

    feat: admin dashboard

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
+            >
+                Admin Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="container px-4 md:px-10">
+                <Link class="p-4 bg-brand-nile-blue-500 text-white shadow-md text-xl" :href="route('admin.trains.index')">Cadastro de Treinos</Link>
+                <!-- <Link :href="route('admin.trains.index')">Treinos</Link> -->
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
