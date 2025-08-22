1ebc3ca58710398995402c8dc69d9068be4b6973
Kataleko <katalekoweb@gmail.com>
2025-08-22 15:34:53 +0100

    feat: creat footer component

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class=" bg-brand-charade-500 text-white py-10 md:py-28">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+
+                <div>
+                    <img src="/assets/images/square-logo.png" alt="">
+                </div>
+
+                <div class="flex flex-col space-y-7 text-lg md:col-span-2">
+                    <div>
+                        <div class="uppercase font-bold">Always Fit &reg;</div>
+                        <div>
+                            Saúde, Logenvidade e Autoestima
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="uppercase font-bold">Contato</div>
+                        <div>
+                            0800 454 5103 <br>
+                            17991226-611 (Whatsapp)
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="uppercase font-bold">Endereço</div>
+                        <div>
+                            Rodovia Fernão Dias, 937 <br>
+                            Complemento: BW Diase - <br>
+                            DocaLog G500 Modulo 5 <br>
+                            CEP: 37646-202 <br>
+                            Extrema - MG
+                            30.417.094/0001-60
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div>
+                    <div class="font-bold uppercase">Política</div>
+                    <div class="mt-5 space-y-5 text-lg">
+                        <div>
+                            <a href="#" class=" underline">Quem Somos</a>
+                        </div>
+                        <div>
+                            <a href="#" class=" underline">Política de Segurança</a>
+                        </div>
+                        <div>
+                            <a href="#" class=" underline">Política de Privacidade</a>
+                        </div>
+                        <div>
+                            <a href="#" class=" underline">Política de Entrega</a>
+                        </div>
+                        <div>
+                            <a href="#" class=" underline">Política de Cookies</a>
+                        </div>
+                        <div>
+                             <a href="#" class=" underline">Política de Devolução</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</template>
